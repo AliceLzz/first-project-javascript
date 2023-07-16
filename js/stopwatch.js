7433896615858bb84cd285b683293b9ffa805027
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-07-16 13:40:42 -0700

    change weather-display to stopwatch (sachika)

---
+++ js/stopwatch.js
@@ -0,0 +1,59 @@
+const timeDisplay = document.getElementById('time');
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const resetBtn = document.getElementById('reset-btn');
+
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let stop = true;
+let inervalId;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+
+startBtn.addEventListener('click', () => {
+    if (stop) {
+        stop = false;
+        startTime = Date.now() - elapsedTime;
+        inervalId = setInterval(updateTime, 75);
+    }
+});
+
+stopBtn.addEventListener('click', () => {
+    if(!stop) {
+        stop = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(inervalId);
+    }
+});
+
+resetBtn.addEventListener('click', () => {
+    stop = true;
+    clearInterval(inervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    timeDisplay.textContent = '00:00:00';
+});
+
+function updateTime(){
+    elapsedTime = Date.now() - startTime;
+
+    secs = Math.floor((elapsedTime / 1000) % 60);
+    mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+    secs = pad(secs);
+    mins = pad(mins);
+    hrs = pad(hrs);
+
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+    function pad(unit) {
+        return (('0') + unit).length > 2 ? unit : '0' + unit;
+    }
+};
