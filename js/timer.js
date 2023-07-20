ca92906257b021f9ebf3b83f447beabacc086dcf
DYagmur <30656517+DYagmur@users.noreply.github.com>
2023-07-20 23:44:55 +0300

    timer commit

--- js/timer.js
+++ js/timer.js
@@ -1,32 +1,73 @@
-function startCountdown() {
-    const hoursInput = document.getElementById('hours');
-    const minutesInput = document.getElementById('minutes');
-    const secondsInput = document.getElementById('seconds');
-    
-    let hours = parseInt(hoursInput.value) || 0;
-    let minutes = parseInt(minutesInput.value) || 0;
-    let seconds = parseInt(secondsInput.value) || 0;
-    
-    let totalSeconds = hours * 3600 + minutes * 60 + seconds;
-    
-    const timerDisplay = document.getElementById('timer');
-    timerDisplay.innerHTML = formatTime(totalSeconds);
-    
-    const countdownInterval = setInterval(() => {
-      totalSeconds--;
-      timerDisplay.innerHTML = formatTime(totalSeconds);
+let countdownInterval;
+let paused = false;
+let pausedSeconds;
 
-      if (totalSeconds <= 0) {
-        clearInterval(countdownInterval);
-        timerDisplay.innerHTML = 'Time\'s up!';
-      }
-    }, 1000);
-  }
-  
-  function formatTime(totalSeconds) {
+function startCountdown() {
+    if (!paused) {
+        const hoursInput = document.getElementById('hours');
+        const minutesInput = document.getElementById('minutes');
+        const secondsInput = document.getElementById('seconds');
+
+        let hours = parseInt(hoursInput.value) || 0;
+        let minutes = parseInt(minutesInput.value) || 0;
+        let seconds = parseInt(secondsInput.value) || 0;
+
+        let totalSeconds = hours * 3600 + minutes * 60 + seconds;
+        pausedSeconds = totalSeconds;
+
+        const timerDisplay = document.getElementById('timer');
+        timerDisplay.innerHTML = formatTime(totalSeconds);
+
+        countdownInterval = setInterval(() => {
+            totalSeconds--;
+
+            if (totalSeconds <= 0) {
+                clearInterval(countdownInterval);
+                timerDisplay.innerHTML = 'Time\'s up!';
+                playBeepSound();
+            } else {
+                timerDisplay.innerHTML = formatTime(totalSeconds);
+            }
+        }, 1000);
+    } else {
+        paused = false;
+        countdownInterval = setInterval(() => {
+            pausedSeconds--;
+
+            if (pausedSeconds <= 0) {
+                clearInterval(countdownInterval);
+                const timerDisplay = document.getElementById('timer');
+                timerDisplay.innerHTML = 'Time\'s up!';
+                playBeepSound();
+            } else {
+                const timerDisplay = document.getElementById('timer');
+                timerDisplay.innerHTML = formatTime(pausedSeconds);
+            }
+        }, 1000);
+    }
+}
+
+function stopCountdown() {
+    clearInterval(countdownInterval);
+    paused = true;
+}
+
+function resetCountdown() {
+    stopCountdown();
+    const timerDisplay = document.getElementById('timer');
+    timerDisplay.innerHTML = '00:00:00';
+    pausedSeconds = 0;
+}
+
+function formatTime(totalSeconds) {
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
-    
+
     return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  }
+}
+
+function playBeepSound() {
+    const beepSound = new Audio('./audio/beep.mp3');
+    beepSound.play();
+}
